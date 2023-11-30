e73c1f168998284fd82b597f393c67ebdda734d1
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-11-30 09:31:16 +0100

    Req

--- server/src/redirect/redirect.controller.ts
+++ server/src/redirect/redirect.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Redirect, Res } from '@nestjs/common';
+import { Controller, Get, Redirect, Req, Res } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('redirector')
 export class RedirectController {
 
     @Get()
-    redirect(@Res() res) {
+    redirect(@Req() req: Request, @Res() res) {
+        console.log(req.params);
         return res.redirect(302, "autopilotapp://redirect?code=1234")
     }
 }
