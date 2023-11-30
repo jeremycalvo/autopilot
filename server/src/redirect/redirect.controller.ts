d7c20ac47f97359acae022db0aad52343bfa92f8
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-11-30 09:03:39 +0100

    test code

--- server/src/redirect/redirect.controller.ts
+++ server/src/redirect/redirect.controller.ts
@@ -5,6 +5,6 @@ export class RedirectController {
 
     @Get()
     redirect(@Res() res) {
-        return res.redirect(302, "autopilotapp://zouzou")
+        return res.redirect(302, "autopilotapp://zouzou?code=1234")
     }
 }
