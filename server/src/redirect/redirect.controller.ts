44963568e00217cb2aadbdc2ece32fc174ef9b80
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-12-04 07:56:32 +0100

    youpi

--- server/src/redirect/redirect.controller.ts
+++ server/src/redirect/redirect.controller.ts
@@ -7,6 +7,6 @@ export class RedirectController {
     @Get()
     redirect(@Req() req: Request, @Res() res) {
         console.log(req.query);
-        return res.redirect(302, "autopilotapp://redirect?code=1234")
+        return res.redirect(302, "autopilotapp://redirect?service=" + req.query.service + "&code=" + req.query.code)
     }
 }
