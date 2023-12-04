631eac59fedbeccf4ffb5f2af1f7a11b309c97c7
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-12-04 08:44:11 +0100

    discord redirect uri

--- server/src/integrations/discord/discord.service.ts
+++ server/src/integrations/discord/discord.service.ts
@@ -29,7 +29,7 @@ export class DiscordService {
             'client_secret': process.env.DISCORD_CLIENT_SECRET,
             'grant_type': 'authorization_code',
             'code': code,
-            'redirect_uri': "http://localhost:8081/services"
+            'redirect_uri': "http://128.199.149.52:8080/redirector?service=discord"
         }
         const headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
