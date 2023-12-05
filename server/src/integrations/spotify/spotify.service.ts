6387468f506cea2c3a566e48421579c2694bc0dd
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-12-05 02:52:21 +0100

    spotify redirect

--- server/src/integrations/spotify/spotify.service.ts
+++ server/src/integrations/spotify/spotify.service.ts
@@ -22,7 +22,7 @@ export class SpotifyService {
             'client_secret': process.env.SPOTIFY_CLIENT_SECRET,
             'grant_type': 'authorization_code',
             'code': code,
-            'redirect_uri': "http://localhost:8081/services"
+            'redirect_uri': "http://128.199.149.52:8080/redirector?service=spotify"
         }
         const headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
