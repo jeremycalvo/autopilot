6cc8684275288223aab5f27663ff6be3b25542fe
Jérémy Calvo <jeremy.calvo@epitech.eu>
2023-12-08 02:36:40 +0100

    debugging

--- server/src/integrations/google/google.service.ts
+++ server/src/integrations/google/google.service.ts
@@ -42,6 +42,7 @@ export class GoogleService {
             this.integrationService.setService(service, req.headers.authorization.split(' ')[1])
             return token;
         } catch (error) {
+            console.log(error);
             return error
         }
     }
